4026796828fd7fce409b1af99a6c9602e505b4ea
ZakirTqvi <husainitaqvi4@gmail.com>
2025-09-04 10:15:44 +0530

    updated

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,22 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-let counter = 12;
-const addValue = () => {
-  console.log("value added", Math.random())
-  counter = counter + 1;
-}
+  let [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const remValue = () => {
+    if (counter > 0){
+       setCounter(counter - 1)
+    }
+   
+  }
   return (
     <>
-      <h1>React Learning</h1>
-      <p>Conter:{counter}</p>
-      <button onClick={addValue}>increase</button><br />
-      <button>decrease</button>
+      <h1>React Counter App</h1>
+      <h2>Conter:{counter}</h2>
+      <button onClick={addValue}>increase</button>
+      <br />
+      <button onClick={remValue}>decrease</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
