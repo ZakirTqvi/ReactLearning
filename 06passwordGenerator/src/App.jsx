1589679c9b7e658f60343c61ca78f1359f10a2e6
ZakirTqvi <husainitaqvi4@gmail.com>
2025-10-02 18:21:38 +0530

    updated

--- 06passwordGenerator/src/App.jsx
+++ 06passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 export default function App() {
   const [length, setLength] = useState(8);
@@ -6,6 +6,8 @@ export default function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
+  // useRef hook
+  const passwordRef = useRef(null)
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -20,6 +22,12 @@ export default function App() {
 
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
+
+ const copyPasswordToClipboard = useCallback( () => {
+     passwordRef.current?.select();
+     passwordRef.current?.setSelectionRange(0, 999);
+     window.navigator.clipboard.writeText(password)
+ }, [password]) 
   
   useEffect(() => {
     passwordGenerator()
@@ -41,8 +49,10 @@ export default function App() {
           className="outline-none w-full py-1 px-3"
           placeholder="Password"
           readOnly
+          ref={passwordRef}
         />
         <button
+          onClick={copyPasswordToClipboard}
           className="outline-none bg-blue-700 text-white
            px-3 py-0.5 shrink-0"
         >
