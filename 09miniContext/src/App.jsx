5d8298957973db0ab8db1407f8517b9d22cb138e
ZakirTqvi <husainitaqvi4@gmail.com>
2025-10-23 21:19:43 +0530

    uploads

---
+++ 09miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+// App.jsx
+
+import UserContextProvider from "./context/UserContextProvider"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import './App.css'
+
+function App() {
+  
+  return (
+    <UserContextProvider>
+      <h1>React Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
